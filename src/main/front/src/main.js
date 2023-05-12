4cadd3ebde11d9d92df30a673e6d8f02a628577d
MeowMeowPuppy <woojw6012@gmail.com>
2023-05-12 15:42:20 +0900

    dependency add
    boot :
     tomcat embed jasper, jstl
     vue3 :
      fontawesome, bootstrap, bootstrap-vue-3

--- src/main/front/src/main.js
+++ src/main/front/src/main.js
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
-createApp(App).use(router).mount('#app')
+/** Bootstrap, Bootstrap-vue-3 */
+import { BootstrapVue3 } from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+/** fontawesome import */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(fab, fas, far)
+
+/** axios 기본 URL */
+axios.defaults.baseURL="http://localhost:8090";
+
+createApp(App)
+.use(router)
+.use(BootstrapVue3)
+.component("font-awesome-icon",FontAwesomeIcon)
+.mount('#app')
--- src/main/front/vue.config.js
+++ src/main/front/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      // axios 사용시 URL을 "/api/" 로 시작하도록 해주세요
+      // 개발 서버의 Mapping URL도 /api/ 로 시작되도록 해주세요
+      "/api/": { 
+        target: "http://localhost:8090",
+        ws : false,
+      }
+    }
+  }
 })
