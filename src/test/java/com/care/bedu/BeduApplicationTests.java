454a5712cec7cec831b284de9d21b78903151050
MeowMeowPuppy <woojw6012@gmail.com>
2023-05-12 15:10:46 +0900

    init project

---
+++ src/test/java/com/care/bedu/BeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.care.bedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
