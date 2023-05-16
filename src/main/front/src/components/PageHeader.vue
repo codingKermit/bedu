3508a29af5b755f02ccb36644b68f33f74c1c110
DESKTOP-QAU4N7T\wasd1 <wasd1223@naver.com>
2023-05-16 16:17:50 +0900

    Revert "no message"
    This reverts commit 4319f34553dcc668704a9994c49cd19f87cf7e72.

--- src/main/front/src/components/PageHeader.vue
+++ src/main/front/src/components/PageHeader.vue
@@ -1,96 +1,93 @@
 <!-- PageHeader.vue -->
 <template>
-  <header>
-    <div class="dien">
+    <header>
       <div id="nav1">
         <router-link to="/">
             <img src="@/assets/imgs/Logo.png" width="150">
         </router-link>
         <router-link to="/">분야별 강의</router-link>
+        <router-link to="/course">분야별 강의</router-link>
+        <div class="search-popup">
+          <form action="#" class="search-form" role="search">
+              <fieldset>
+                <div class="search-form-con">
+                  <input type="text" name="search">
+                </div>
+              </fieldset>
+          </form>
+        </div>
+      </div>
+      <div id="nav2">
         <router-link to="/community">커뮤니티</router-link>
         <router-link to="/review">수강후기</router-link>
         <router-link to="/companyStudy">기업교육</router-link>
-      </div>
-      <div id="nav2">
-        <div class="search-popup">
-            <div class="border-3 rounded-pill p-2 d-flex align-middle text-center m-auto mb-5 border-bedu">
-              <font-awesome-icon class="m-auto mx-3" :icon="['fas', 'magnifying-glass']" />
-              <b-form-input class="border-0 me-2"></b-form-input>
-            </div>
-        </div>
         <router-link to="/login">로그인</router-link>
         <router-link to="/regist">회원가입</router-link>
       </div>
-    </div>
-  </header>
+    </header>
 </template>
 
-<script>
-export default {
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+  header {
+    margin: 0 auto;
+    background-color: #EDC268;
+    padding-top: 3%;
+    padding-bottom: 5%;
+    display: flex;
+    justify-content: space-between;
+  }
 
-}
-</script>
+  #nav1 {
+    display: flex;
+    align-items: center;
+  }
 
-<style scoped>
-header {
-margin: 0 auto;
-background-color: #EDC268;
-padding-top: 3%;
-padding-bottom: 5%;
-}
+  #nav1 img {
+    margin-right: 10px;
+  }
 
-.dien {
-width: auto;
-}
+  #nav1 a {
+    font-family: 'Noto Sans CJK KR', sans-serif;
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+    margin-left: 20px;
+  }
 
-.dien #nav1 {
-float: left;
-}
+  .search-form {
+    margin-left: 70px;
+  }
 
-.dien #nav2 {
-float: right;
-}
+  .search-form-con {
+    display: flex;
+    align-items: center;
+  }
 
-#nav1 img {
-margin-right: 20px;
-}
+  .search-form-con input {
+    width: 100%;
+    height: 40px;
+    padding: 5px 10px;
+    border: none;
+    background-color: #F6E0B3;
+    border-radius: 30px;
+  }
 
-#nav1 a {
-margin-right: 30px;
-font-family: 'Noto Sans CJK KR', sans-serif;
-font-weight: bold;
-color: #2c3e50;
-text-decoration: none;
-}
+  #nav2 {
+    display: flex;
+    align-items: center;
+  }
 
-#nav2 a {
-margin-left: 20px;
-font-family: 'Noto Sans CJK KR', sans-serif;
-font-weight: bold;
-color: #2c3e50;
-text-decoration: none;
-}
-
-#nav2 .search-popup {
-display: inline-block;
-margin-left: -20px;
-margin-right: -50px;
-}
-
-#nav2 .search-popup input {
-width: 100%;
-height: 15px;
-padding: 5px 10px;
-border: none;
-background-color: #F6E0B3;
-border-radius: 30px;
-}
-
-.border-bedu{
-background-color: #F6E0B3;
-width: 70%;
-border-color: #EDC268;
-}
-
-
+  #nav2 a {
+    font-family: 'Noto Sans CJK KR', sans-serif;
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+    margin-left: 20px;
+  }
 </style>
