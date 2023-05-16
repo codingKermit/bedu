fd07f2d40dd054a74a98d6921c8fe12dfecf8145
MeowMeowPuppy <woojw6012@gmail.com>
2023-05-16 12:59:33 +0900

    MainView -> HomeView

--- src/main/front/src/router/index.js
+++ src/main/front/src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
   {
     path: '/',
     name: 'main',
-    component: () => import('../views/MainView.vue')
+    component: () => import('../views/HomeView.vue')
   },
   {
     path: '/course',
