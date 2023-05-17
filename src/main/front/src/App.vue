616e6bc1811a2cbae1da844045efe1cc3da4e0f2
yppal29 <yppal29@naver.com>
2023-05-17 10:05:50 +0900

    HeaderFix

--- src/main/front/src/App.vue
+++ src/main/front/src/App.vue
@@ -1,46 +1,18 @@
 <template>
-  <div class="side">
-    <PageHeader/> <!-- 헤더 컴포넌트 -->
-    <div class="body">
-      <router-view/> <!-- 페이지 이동이 표시될 곳 -->
-    </div>
-    <PageFooter/> <!-- 푸터 컴포넌트 -->
-    <b-link :to="test"></b-link>
-  </div>
+    <BeduView/>
 </template>
 
 <script>
-import PageHeader from '@/components/PageHeader'
-import PageFooter from '@/components/PageFooter'
+import BeduView from '@/components/BeduView'
 
 export default {
   name: 'App',
   components: {
-    PageFooter,
-    PageHeader
+    BeduView
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Noto Sans KR' ,'NanumMyeongjo', 'Times';
-}
-
-.body {
-  margin: 0px;
-  padding: 0px;
-  background-color: white;
-}
-
-.side {
-    padding-left: 6%;
-    padding-right: 6%;
-    background-color: #EDC268;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 
 </style>
