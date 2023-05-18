346d638b4804a29e2c5c21cf5a49a7a98f78c6a3
hyunjong <guswhd4321@naver.com>
2023-05-18 17:31:40 +0900

    푸터 수정 및 헤더 수정본

--- src/main/front/src/components/PageFooter.vue
+++ src/main/front/src/components/PageFooter.vue
@@ -1,18 +1,38 @@
 <!-- PageFooter.vue -->
 <template>
   <footer>
-    <div id="footer">
-      <router-link to="/tou">이용약관</router-link>
-      <router-link to="/privacy">개인정보 보호 방침</router-link>
-      <router-link to="/csc">고객센터</router-link>
-      <img src="../assets/imgs/test.png">
-      <p>
-        <span>(주)Test 통신판매업번호: 제 2019-서울강남-01371호</span><br/>
-        <span>평생교육시설 신고: 제원-408호 Test 사이버평생교육원</span><br/>
-        <span>Email: test@test.com FAX: 02-1234-1234 사업자번호: 123-12-1234 대표자: test 개인정보책임자 : test</span><br/>
-        <span>대표전화: 1234-1234</span><br/>
-        <span>주소: 서울시 강남구 강남대로 test</span>
-      </p>
+    <div class="bedu-ft">
+      <div class="bedu-ft-cate-links">
+        <div class="bedu-ft-cate-links-lt">
+          <div class="item">
+            <router-link class="bedu-ft-cate" to="">비에듀</router-link>            
+          </div>
+          <div class="item">
+            <router-link class="bedu-ft-cate" to="">기업교육</router-link>
+          </div>
+          <div class="item">
+            <router-link class ="bedu-ft-cate" to="/tou">사용자 이용약관</router-link>
+          </div>
+          <div class="item">
+            <router-link class="bedu-ft-cate" to="/privacy">개인정보 보호 방침</router-link>
+          </div>
+          <div class="item">
+            <router-link class="bedu-ft-cate" to="/csc">고객센터</router-link>
+          </div>
+          <div class="item">
+            <router-link class="bedu-ft-cate" to="">코드등록</router-link>
+          </div>
+          <!-- <img src="../assets/imgs/test.png"> -->
+        </div>
+      </div>
+      <div class="bedu-ft-des-details">
+        <div class="bedu-ft-des">
+          <p><span>(주)Test 통신판매업번호: 제 2019-서울강남-01371호</span></p>
+          <p><span>평생교육시설 신고: 제원-408호 Test 사이버평생교육원</span></p>
+          <p><span>Email: test@test.com FAX: 02-1234-1234 사업자번호: 123-12-1234 대표자: test 개인정보책임자 : test 대표전화: 1234-1234</span></p>
+          <p><span>주소: 서울시 강남구 강남대로 test</span></p>
+        </div>
+      </div>  
     </div>
   </footer>
 </template>
@@ -24,24 +44,53 @@ export default {}
 <style scoped>
 footer {
   margin: 0 auto;
-  padding-top: 3%;
-  padding-bottom: 5%;
+  padding: 3rem 0 5rem 0;
   text-align: left;
 }
 
 footer img {
   float: left;
-  height: 130px;
-  width: 150px;
+  height: 13px;
+  width: 10px;
   margin: 27px;
 }
 
-footer a {
-  margin-right: 10px;
+.bedu-ft{
+  display:grid;
+  grid-template-areas: "links" "details" "icons";
+  justify-content: center;
+  width:100%;
+  max-width: 110rem;
+  margin:0 auto;
 }
-
-footer p {
-  margin-top: 10px;
+.bedu-ft-cate-links{
+  grid-area: links;
+}
+.bedu-ft-cate-links-lt{
+  display:flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  column-gap: 2.4rem;
+  row-gap: 0.5rem;
+}
+.bedu-ft-cate {
+  font-size: 1.2rem;
+  font-family: 'Noto Sans CJK KR', sans-serif;
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+}
+.bedu-ft-des-details{
+  grid-area: details;
+  max-width: 70rem;
+  margin-top: 2rem;
+}
+.bedu-ft-des{
+  display:flex;
+  min-width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 
 </style>
