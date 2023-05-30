b693e6d803851d7af89e61e83a165a28c295ccab
DESKTOP-0L9I7OA\WJY <whg98@naver.com>
2023-05-30 17:04:33 +0900

    Revert "통계화면 추가"
    This reverts commit 6993e7bae7835fc0a35d2bf6dad667dfa786a6fe.

--- src/main/front/src/router/index.js
+++ src/main/front/src/router/index.js
@@ -131,11 +131,6 @@ const routes = [
     name: 'reviewWrite',
     component: () => import('../views/Review/ReviewWrite.vue')
   },
-  {
-    path: '/StatisticsMain',
-    name: 'StatisticsMain',
-    component:() => import('../views/statistics/StatisticsMain.vue')
-  },
 ]
 
 const router = createRouter({
