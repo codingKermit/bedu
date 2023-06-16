4d50e53541630192b2ee152afab78896924401e5
juyoung1222 <jjurong1222@gmail.com>
2023-06-16 13:43:27 +0900

    마이페이지 화면 개발중

--- src/main/front/src/views/user/MypageView.vue
+++ src/main/front/src/views/user/MypageView.vue
@@ -1,5 +1,65 @@
+<!-- npm install v-pagination-3 설치해야함 -->
 <template>
-    <div class="mypage">
-        <h2>마이페이지 입니다.</h2>
-    </div>
+    <b-container>
+        <p class="fs-4 fw-bold">현재 수강정보</p>
+            <div style="height: 100px; width: 100%; background-color: var(--yellow);" class="rounded-6">
+                <b-container class="text-white fw-bold d-flex">
+                    <div class="curr-subjectInfo" >
+                        <p>자바</p>
+                        <p>파이썬</p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </b-container>
+            </div>
+        </b-container>
+        <b-container>
+            <p class="fs-4 fw-bold">현재 수강정보</p>
+            <div style="height: 100px; width: 100%; background-color: var(--yellow);" class="rounded-6">
+                <b-container class="text-white fw-bold d-flex">
+                    <div class="curr-subjectInfo">
+                        <!--<p>자바</p>
+                        <p>파이썬</p>
+                        <p></p>
+                        <p></p>
+                        <p></p>-->
+                        <p>수강내역이 없습니다.</p>
+                    </div>
+                </b-container>
+            </div>
+        </b-container>
+        <b-container>
+            <p class="fs-4 fw-bold">북마크</p>
+            <div style="height: 100px; width: 100%; background-color: var(--yellow);" class="rounded-7">
+                <b-container class="text-white fw-bold d-flex">
+                    <div class="bookmark" >
+                        <p>자바</p>
+                        <p>파이썬</p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </b-container>
+            </div>
+        </b-container>
+        <b-container>
+            <p class="fs-4 fw-bold">북마크</p>
+            <div style="height: 100px; width: 100%; background-color: var(--yellow);" class="rounded-7">
+                <b-container class="text-white fw-bold d-flex">
+                    <div class="bookmark" >
+                       <!-- <p>자바</p>
+                        <p>파이썬</p>
+                        <p></p>
+                        <p></p>
+                        <p></p>-->
+                        <p>해당 강의 북마크가 없습니다.</p>
+                    </div>
+                </b-container>
+            </div>
+        </b-container>
 </template>
+<script>
+
+</script>
+
