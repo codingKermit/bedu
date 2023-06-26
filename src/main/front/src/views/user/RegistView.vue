3a3f268d22eb79b00afd4788cfc3ba1e272c0e52
yppal29 <yppal29@naver.com>
2023-06-26 16:13:43 +0900

    로그인, 회원가입 주석, 수정 완료

--- src/main/front/src/views/user/RegistView.vue
+++ src/main/front/src/views/user/RegistView.vue
@@ -2,9 +2,11 @@
     <div id="registChangeContainer">
         <!-- 로그인 및 회원가입 이동 버튼 -->
         <div id="registChangeSection">
+            <!-- 로그인 버튼 -->
             <button @click="$router.push('/login')" id="reLoginChangeBtn">
                 로그인
             </button>
+            <!-- 회원가입 버튼 -->
             <button @click="$router.push('/regist')" id="reRegistChangeBtn">
                 회원가입
             </button>
@@ -229,14 +231,12 @@
                         <div id="registFormGroup">
                             <input
                                 type="password"
-                                id="registPassword"
+                                id="registConfirmPassword"
                                 placeholder="비밀번호 입력 확인"
                                 v-model="confirmPassword"
                             />
                             <p
                                 v-show="
-                                    valid.password &&
-                                    member.password &&
                                     member.password !== confirmPassword
                                 "
                                 id="registInputError"
@@ -245,7 +245,7 @@
                             </p>
                             <p
                                 v-show="
-                                    valid.password &&
+                                    !valid.password &&
                                     member.password &&
                                     member.password == confirmPassword &&
                                     member.password.length >= 6 &&
@@ -274,6 +274,7 @@
 </template>
 
 <script>
+    // 필요한 모듈 및 CSS 파일을 가져옵니다.
     import '@/assets/css/userStyle.css';
     export default {
         name: 'EmailInput',
@@ -291,34 +292,30 @@
                     },
                     { value: 3, label: "이벤트 수신 동의", optional: true },
                 ],
-                // fileText1 및 fileText2는 각각 이용약관과 개인정보 수집에 대한 내용을 담은 텍스트를 저장하는 변수입니다.
-                fileText1: '',
-                fileText2: '',
-                fileText3: '',
-                showForm: false, // showForm은 회원가입 양식을 보여줄지 여부를 결정하는 데이터 속성
-                confirmPassword: "", //  비밀번호 확인을 위해 입력된 값을 저장하는 변수입니다.
-                // 중복 체크 상태를 나타내는 변수들입니다.
-                isChecking: false,
-                emailChecked: false,
-                nickChecked: false,
-                emailValue: '',
-                emailList: [],
-                // 회원의 이메일, 닉네임, 비밀번호를 저장하는 객체입니다.
+                fileText1: '', // 이용약관에 대한 내용을 담을 변수
+                fileText2: '', // 개인정보 수집에 대한 내용을 담을 변수
+                fileText3: '', // 이벤트 수신에 대한 내용을 담을 변수
+                showForm: false, // 회원가입 양식을 보여줄지 여부를 결정하는 데이터 속성
+                confirmPassword: "", // 비밀번호 확인을 위해 입력된 값을 저장하는 변수
+                isChecking: false, // 중복 체크 상태를 나타내는 변수
+                emailChecked: false, // 이메일 중복 체크 상태를 나타내는 변수
+                nickChecked: false, // 닉네임 중복 체크 상태를 나타내는 변수
+                emailValue: '', // 이메일 입력 필드의 값
+                emailList: [], // 이메일 목록을 담을 배열
                 member: {
-                    email: "",
-                    nickname: "",
-                    password: "",
+                    email: "", // 회원의 이메일
+                    nickname: "", // 회원의 닉네임
+                    password: "", // 회원의 비밀번호
                 },
-                showEmailList: false, // 이메일 목록 표시 여부를 나타내는 변수입니다.
-                filteredEmailList: [], // 필터링된 이메일 목록을 저장하는 변수입니다.
-                // 이메일, 닉네임, 비밀번호 등의 유효성을 나타내는 변수들입니다.
+                showEmailList: false, // 이메일 목록 표시 여부를 나타내는 변수
+                filteredEmailList: [], // 필터링된 이메일 목록을 저장하는 변수
                 valid: {
-                    email: false,
-                    nickname: false,
-                    password: false,
-                    emailChk: false,
-                    nickChk: false,
-                    emailDomain: false
+                    email: false, // 이메일의 유효성 검사 결과
+                    nickname: false, // 닉네임의 유효성 검사 결과
+                    password: false, // 비밀번호의 유효성 검사 결과
+                    emailChk: false, // 이메일 중복 체크 결과
+                    nickChk: false, // 닉네임 중복 체크 결과
+                    emailDomain: false // 이메일 도메인 유효성 검사 결과
                 },
             };
         },
@@ -332,7 +329,6 @@
             "member.password": function () {
                 this.checkPassword();
             },
-
             emailValue(value) {
                 this.member.email = value;
                 const frequencyEmails = [
@@ -345,30 +341,31 @@
                     '@nate.com',
                     '@kakao.com',
                 ];
-
                 const userEmails = frequencyEmails.map((email) => {
                     return value.includes('@') ? value.split('@')[0] + email : value + email;
                 });
-
                 this.emailList = userEmails;
             },
         },
         methods: {
+            // 이메일 입력 시 이메일 목록을 보여주는 메서드
             showEmailOptions() {
                 if (this.emailValue) {
-                // 이메일 목록을 필터링하여 보여줄 목록을 설정합니다.
-                this.filteredEmailList = this.emailList.filter((email) =>
-                    email.includes(this.emailValue)
-                );
-                this.showEmailList = true;
+                    // 이메일 목록을 필터링하여 보여줄 목록을 설정합니다.
+                    this.filteredEmailList = this.emailList.filter((email) =>
+                        email.includes(this.emailValue)
+                    );
+                    this.showEmailList = true;
                 } else {
-                this.showEmailList = false;
+                    this.showEmailList = false;
                 }
             },
+            // 선택된 이메일 옵션을 emailValue에 할당하고 이메일 목록을 숨깁니다.
             selectEmailOption(email) {
-                this.emailValue = email; // 이메일 값을 emailValue에 할당하여 <input> 요소에 반영합니다.
+                this.emailValue = email;
                 this.showEmailList = false;
             },
+            // 약관 동의 항목을 조회하는 메서드
             fetchAgreements() {
                 const ids = [0, 1, 2]; // 변경할 id 값들을 배열로 선언
 
@@ -376,13 +373,12 @@
                 ids.forEach(id => {
                     this.$axiosSend("get", `/api/agree/${id}`)
                     .then(response => {
-                        // console.log(response.data[0].content);
                         if (id === 0) {
-                        this.fileText1 = response.data[0].content;
+                            this.fileText1 = response.data[0].content;
                         } else if (id === 1) {
-                        this.fileText2 = response.data[0].content;
+                            this.fileText2 = response.data[0].content;
                         } else if (id === 2) {
-                        this.fileText3 = response.data[0].content;
+                            this.fileText3 = response.data[0].content;
                         }
                     })
                     .catch(error => {
@@ -390,30 +386,24 @@
                     });
                 });
             },
-            // 이메일의 유효성을 검사하는 메서드입니다. 정규식을 사용하여 이메일 형식을 확인하고 valid.email과 emailChecked 값을 업데이트합니다.
+            // 이메일의 유효성을 검사하는 메서드
             checkEmail() {
-                const validateEmail =
-                    /^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\.[A-Za-z0-9\\-]+/;
-
+                const validateEmail = /^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\.[A-Za-z0-9\\-]+/;
                 if (!this.member.email) {
                     this.valid.email = false;
                     this.emailChecked = false;
                     return;
                 }
-
                 if (!validateEmail.test(this.member.email)) {
                     this.valid.email = true;
                     this.emailChecked = false;
                     this.valid.emailDomain = false;
                     return;
                 }
-
                 this.valid.email = false;
                 this.valid.emailChk = false;
                 this.emailChecked = false;
-
                 const domain = this.member.email.split('@')[1];
-
                 const allowedDomains = [
                     'naver.com',
                     'gmail.com',
@@ -424,10 +414,9 @@
                     'nate.com',
                     'kakao.com',
                 ];
-
                 this.valid.emailDomain = allowedDomains.includes(domain);
             },
-            // 이메일 중복 체크를 수행하는 메서드입니다. axios를 사용하여 서버에 이메일 중복 여부를 요청하고 결과에 따라 emailChecked와 valid.emailChk 값을 업데이트합니다.
+            // 이메일 중복 체크를 수행하는 메서드
             checkEmailDuplicate() {
                 this.isChecking = true;
                 this.$axiosSend("get", `/api/register/email/${this.member.email}`)
@@ -444,27 +433,24 @@
                     this.isChecking = false;
                 });
             },
-            // 네임의 유효성을 검사하는 메서드입니다. 정규식을 사용하여 닉네임 형식을 확인하고 valid.nickname과 nickChecked 값을 업데이트합니다.
+            // 닉네임의 유효성을 검사하는 메서드
             checkNick() {
                 const validateNick = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,10}$/;
-
                 if (!this.member.nickname) {
                     this.valid.nickname = false;
                     this.nickChecked = false;
                     return;
                 }
-
                 if (!validateNick.test(this.member.nickname)) {
                     this.valid.nickname = true;
                     this.nickChecked = false;
                     return;
                 }
-
                 this.valid.nickname = false;
                 this.valid.nickChk = false;
                 this.nickChecked = false;
             },
-            // 닉네임 중복 체크를 수행하는 메서드입니다. axios를 사용하여 서버에 닉네임 중복 여부를 요청하고 결과에 따라 nickChecked와 valid.nickChk 값을 업데이트합니다.
+            // 닉네임 중복 체크를 수행하는 메서드
             checkNickDuplicate() {
                 this.isChecking = true;
                 this.$axiosSend(
@@ -484,22 +470,21 @@
                     this.isChecking = false;
                 });
             },
-            // 비밀번호의 유효성을 검사하는 메서드입니다. 정규식을 사용하여 비밀번호 형식을 확인하고 valid.password 값을 업데이트합니다.
+            // 비밀번호의 유효성을 검사하는 메서드
             checkPassword() {
                 const validatePassword = /^.{6,15}$/;
                 if (
                     !validatePassword.test(this.member.password) ||
                     !this.member.password ||
                     this.member.password.length < 6 ||
-                    this.member.password.length > 15 ||
-                    this.member.password !== this.confirmPassword
+                    this.member.password.length > 15
                 ) {
                     this.valid.password = true;
                     return;
                 }
                 this.valid.password = false;
             },
-            // "모든 이용 약관에 동의" 체크박스 토글(1개이상 체크박스 체크 빠질시 "모든 이용 약관에 동의" 체크박스 해제)
+            // "모든 이용 약관에 동의" 체크박스 토글 메서드
             toggleAllAgreements(event) {
                 this.selectedAgreements = event.target.checked
                     ? this.agreements.map((item) => item.value)
@@ -511,17 +496,18 @@
                 this.allChecked =
                     this.selectedAgreements.length === this.agreements.length;
             },
-            // 개별 약관 체크박스 토글
+            // 개별 약관 체크박스 토글 메서드
             toggleCheckbox(value) {
                 const checkbox = document.getElementById(`agree_${value}`);
                 checkbox.checked = !checkbox.checked;
                 this.updateAllChecked();
             },
+            // 약관 동의 버튼 클릭 시 처리하는 메서드
             handleAgreementSubmit() {
                 // 스크롤을 페이지 상단으로 이동
                 window.scrollTo({
                     top: 0,
-                    behavior: 'smooth', // 스무스한 스크롤링 효과
+                    behavior: 'smooth',
                 });
                 this.showForm = true;
             },
@@ -529,8 +515,7 @@
             showRegistrationForm() {
                 this.showForm = true;
             },
-            // 사용자의 회원가입을 처리하는 메서드입니다. 이메일, 닉네임, 비밀번호의 유효성을 확인하고
-            // 중복 체크를 완료한 경우 서버에 회원가입 요청을 보냅니다. 회원가입에 성공하면 로그인 페이지로 이동합니다.
+            // 사용자의 회원가입을 처리하는 메서드
             register() {
                 if (
                     !this.emailChecked ||
@@ -544,9 +529,9 @@
                 ) {
                     this.$swal("닉네임을 다시 확인해주세요.");
                 } else if (
-                    !this.valid.password ||
+                    this.valid.password ||
                     !this.member.password ||
-                    this.member.password != this.confirmPassword ||
+                    this.member.password !== this.confirmPassword ||
                     this.member.password.length < 6 ||
                     this.member.password.length > 15
                 ) {
@@ -557,17 +542,15 @@
                     this.valid.emailDomain &&
                     this.nickChecked &&
                     this.valid.nickChk &&
-                    this.valid.password &&
+                    !this.valid.password &&
                     this.member.password &&
                     this.member.password == this.confirmPassword &&
                     this.member.password.length >= 6 &&
                     this.member.password.length <= 15
                 ) {
-                    // '/api/register'주소로 email,nickname,password를 member에 담아 보냄
                     this.$axiosSend('post', '/api/register', this.member)
                     .then((response) => {
                         this.$swal(response.data);
-                        // 문제없이 보냈다면 'login'으로 router이동
                         this.$router.push({
                             name: "login",
                         });
@@ -585,10 +568,8 @@
         created() {
             this.fetchAgreements(); // 컴포넌트 생성 시 약관 동의 항목과 내용을 조회
         },
-
-        // 약관 동의 여부에 따라 Submit 버튼의 스타일을 변경하는 속성
         computed: {
-            // 필수 약관에 동의한 경우 '동의합니다' 버튼 색변경과 텍스트색 변경
+            // 약관 동의 여부에 따라 Submit 버튼의 스타일을 변경하는 계산된 속성
             submitButtonColor() {
                 return this.selectedAgreements.includes(1) &&
                     this.selectedAgreements.includes(2)
--- src/main/front/src/vuex/actions.js
+++ src/main/front/src/vuex/actions.js
@@ -1,54 +1,69 @@
-import {EMAIL, IS_AUTH, ERROR_STATE, NICKNAME, USERNUM, CLS} from './mutation_types'
+import { EMAIL, IS_AUTH, ERROR_STATE, NICKNAME, USERNUM, CLS } from './mutation_types'
 import loginAPI from './loginAPI'
 import jwt_decode from 'jwt-decode'
 
+// 이메일을 설정하는 액션
 let setEmail = ({ commit }, data) => {
     commit(EMAIL, data)
 }
 
+// 에러 상태를 설정하는 액션
 let setErrorState = ({ commit }, data) => {
     commit(ERROR_STATE, data)
 }
 
+// 인증 상태를 설정하는 액션
 let setIsAuth = ({ commit }, data) => {
     commit(IS_AUTH, data)
 }
 
+// 닉네임을 설정하는 액션
 let setNickname = ({ commit }, data) => {
     commit(NICKNAME, data)
 }
 
+// 사용자 번호를 설정하는 액션
 let setUsernum = ({ commit }, data) => {
     commit(USERNUM, data)
 }
 
+// 클라스를 설정하는 액션
 let setCls = ({ commit }, data) => {
     commit(CLS, data)
 }
 
-// 백엔드에서 반환한 결과값을 가지고 로그인 성공 실패 여부를 vuex에 넣어준다.
+// 백엔드에서 반환한 결과값을 처리하여 로그인 성공 여부를 Vuex에 저장하는 함수
 let processResponse = (store, loginResponse) => {
     let decodedToken;
     switch (loginResponse) {
         case 'notFound':
-        setErrorState(store, 'Wrong ID or Password')
-        setIsAuth(store, false)
-        break
+            // ID 또는 비밀번호가 잘못되었음을 에러 상태에 저장
+            setErrorState(store, 'Wrong ID or Password')
+            // 인증 상태를 실패로 설정
+            setIsAuth(store, false)
+            break
         default:
-        decodedToken = jwt_decode(loginResponse.data.user_token);
-        setUsernum(store, decodedToken.usernum)
-        setNickname(store, decodedToken.nickname)
-        setEmail(store, decodedToken.email)
-        setCls(store, decodedToken.cls)
-        setErrorState(store, '')
-        setIsAuth(store, true)
+            decodedToken = jwt_decode(loginResponse.data.user_token);
+            // 토큰을 해독하여 사용자 번호, 닉네임, 이메일, 클라스를 설정
+            setUsernum(store, decodedToken.usernum)
+            setNickname(store, decodedToken.nickname)
+            setEmail(store, decodedToken.email)
+            setCls(store, decodedToken.cls)
+            // 에러 상태를 초기화
+            setErrorState(store, '')
+            // 인증 상태를 성공으로 설정
+            setIsAuth(store, true)
     }
 }
 
 export default {
-    async login(store, {email, password}) {
+    // 로그인 액션
+    async login(store, { email, password }) {
+        // 로그인 API 호출
         let loginResponse = await loginAPI.doLogin(email, password)
+        // 백엔드 응답 처리
         processResponse(store, loginResponse)
-        return store.getters.getIsAuth  // 로그인 결과를 리턴한다
+        // 로그인 결과 반환
+        return store.getters.getIsAuth
     },
 }
