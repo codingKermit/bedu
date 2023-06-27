798d5083fd1dd04a722e357d923d2c113216b745
yppal29 <yppal29@naver.com>
2023-06-27 17:22:50 +0900

    로그아웃 confirm버튼 색 변경

--- src/main/front/src/components/PageHeader.vue
+++ src/main/front/src/components/PageHeader.vue
@@ -174,7 +174,6 @@
                     title: '로그아웃 하시겠습니까?',
                     
                     showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
-                    confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
                     cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
                     confirmButtonText: '확인', // confirm 버튼 텍스트 지정
                     cancelButtonText: '취소', // cancel 버튼 텍스트 지정
