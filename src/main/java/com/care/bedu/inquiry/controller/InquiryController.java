aeae520bae266233776f906b73886904fa6cefc9
DESKTOP-75U5705\dydgk <a01025932320@gmail.com>
2023-07-03 10:10:34 +0900

    inquiry 부분 삭제

--- src/main/java/com/care/bedu/inquiry/controller/InquiryController.java
+++ src/main/java/com/care/bedu/inquiry/controller/InquiryController.java
@@ -19,26 +19,4 @@ import com.care.bedu.inquiry.vo.InquiryVo;
 @RequestMapping("/api")
 public class InquiryController {
 	
-	@Autowired
-	private InquiryService inquiryService;
-	
-	  @RequestMapping("/inquiry/inquiryList")
-	    public List<HashMap<String, Object>> getAllInquiry(@RequestParam int page, @RequestParam int size) {
-	        return inquiryService.getAllInquiry(page, size);
-	    }
-	    
-	    // GET 요청을 처리하는 엔드포인트로, 특정 문의를 가져옵니다.
-
-	    // POST 요청을 처리하는 엔드포인트로, 새로운 문의를 생성합니다.
-	    // 요청 본문에 있는 Inquiry 객체를 받아서 문의를 생성하고 생성된 후기를 반환합니다.
-	    @RequestMapping("/inquiry/inquiryWrite")
-	    public InquiryVo createInquiry(@RequestBody InquiryVo inquiryVo) {
-	        return inquiryService.createInquiry(inquiryVo);
-	    }
-	    // GET 요청을 처리하는 엔드포인트로, 검색된 문의 목록을 가져옵니다.
-	    // keyword는 요청 매개변수로 받아서 검색에 사용합니다.
-	    @RequestMapping("/inquiry/inquirysearch")
-	    public List<HashMap<String, Object>> getSearchedReviews(@RequestParam String keyword) {
-	        return inquiryService.getSearchedInquiry(keyword);
-	    }
 	}
--- src/main/java/com/care/bedu/inquiry/service/serviceImpl/InquiryServiceImpl.java
+++ src/main/java/com/care/bedu/inquiry/service/serviceImpl/InquiryServiceImpl.java
@@ -14,47 +14,5 @@ import com.care.bedu.inquiry.vo.InquiryVo;
 @Service
 public class InquiryServiceImpl implements InquiryService{
 	
-	private InquiryDao inquiryDao;
-	
-	  @Autowired
-	    public InquiryServiceImpl(InquiryDao inquiryDao) {
-	        this.inquiryDao = inquiryDao;
-	        
-	    }
-	    
-	    @Override
-	    public List<HashMap<String, Object>> getAllInquiry(int page, int size) {
-	        int startIndex = (page - 1) * size;
-	        int endIndex = startIndex + size;
-	        List<HashMap<String, Object>> allReviews = inquiryDao.getAllInquiry();
-	        if (startIndex > Math.min(endIndex, allReviews.size())) {
-	            return new ArrayList<>(); // 시작 인덱스가 문의 목록의 범위를 벗어나면 빈 리스트를 반환합니다.
-	        }
 
-	        return allReviews.subList(startIndex, Math.min(endIndex, allReviews.size()));
-	    }
-	    
-	    @Override
-	    public InquiryVo createInquiry(InquiryVo inquiryVo) {
-	        // 문의를 생성하는 로직입니다.
-	    	inquiryDao.createInquiry(inquiryVo);
-	        return inquiryVo;
-	    }
-
-	    
-	    @Override
-	    public List<HashMap<String, Object>> getSearchedInquiry(String keyword) {
-	      // 검색 키워드에 기반하여 문의 필터링
-	      List<HashMap<String, Object>> allInquiry = inquiryDao.getAllInquiry();
-	      List<HashMap<String, Object>> searchedInquiry = new ArrayList<>();
-	      for (HashMap<String, Object> inquiry : allInquiry) {
-	        String title = (String) inquiry.get("TITLE");
-	        String content = (String) inquiry.get("CONTENT");
-	        if (title.toLowerCase().contains(keyword) ||
-	            content.toLowerCase().contains(keyword)) {
-	          searchedInquiry.add(inquiry);
-	        }
-	      }
-	      return searchedInquiry;
-	    }
 	}
