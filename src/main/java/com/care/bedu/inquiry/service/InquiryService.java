ba76cc7025dbe7c7cf33e2793482517ae65c965b
DESKTOP-75U5705\dydgk <a01025932320@gmail.com>
2023-07-03 13:18:50 +0900

    파일명 수정 변경

--- src/main/java/com/care/bedu/inquiry/service/InquiryService.java
+++ src/main/java/com/care/bedu/inquiry/service/InquiryService.java
@@ -3,10 +3,15 @@ package com.care.bedu.inquiry.service;
 import java.util.HashMap;
 import java.util.List;
 
-import com.care.bedu.inquiry.vo.InquiryVo;
-
+import com.care.bedu.inquiry.vo.InquiryVO;
 
 public interface InquiryService {
-	
-}
 
+	// 모든 문의를 가져오는 메서드로, page와 size 매개변수로 페이징 처리를 수행합니다.
+	List<HashMap<String, Object>> getAllInquiry(int page, int size);
+
+	// 문의를 생성하는 메서드입니다.
+	InquiryVO createInquiry(InquiryVO inquiryVO);
+
+	List<HashMap<String, Object>> getSearchedInquiry(String keyword);
+}
--- src/main/java/com/care/bedu/inquiry/vo/InquiryVO.java
+++ src/main/java/com/care/bedu/inquiry/vo/InquiryVO.java
@@ -1,18 +1,16 @@
 package com.care.bedu.inquiry.vo;
 
-import java.util.Date;
-
 import lombok.Data;
 
 @Data
 public class InquiryVO {
-	
-	private int voc_num; //글번호
-	private String title; //제목
-	private String content; //내용
-	//private String FILE_NAME; //.파일 왼쪽 부분
-	//private String FILE_TYPE; //.파일 우측 부분
+
+	private int voc_num; // 글번호
+	private String title; // 제목
+	private String content; // 내용
+	private String FILE_NAME; // .파일 왼쪽 부분
+	private String FILE_TYPE; // .파일 우측 부분
 	private String USER_NAME; // 유저 이름
-	private String reg_date;	//문의 시간
-	private String reg_id;	// 타 테이블에서 구분
+	private String reg_date; // 문의 시간
+	private String reg_id; // 타 테이블에서 구분
 }
