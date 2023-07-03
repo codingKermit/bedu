9b6f1758ef6d758a8bd592b485c427ba3ba92c95
juyoung1222 <jjurong1222@gmail.com>
2023-07-03 15:38:02 +0900

    0703 마이페이지 전체보기 선택시 보이는 화면 개발중

---
+++ src/main/front/src/views/user/MypageUserLectureDetail.vue
@@ -0,0 +1,23 @@
+<!-- npm install v-pagination-3 설치해야함 -->
+<template>
+    <b-container>
+        <div class="contents">
+            <div class="title-div">
+                <span class="title title-3 text-ellipsis title-nickname">
+                    {{ nickname }} 님의 현재 수강상세정보
+                </span>
+            </div>
+        </div>
+    </b-container>
+</template>
+<script>
+export default {
+    name : "mypageAll",
+    data() {
+       return {
+           
+        }
+    }
+}
+
+</script>
