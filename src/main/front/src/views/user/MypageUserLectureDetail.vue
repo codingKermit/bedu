99d008fb0a377fbbea4a7494bac5c8b2e24c88ce
juyoung1222 <jjurong1222@gmail.com>
2023-07-04 16:29:17 +0900

    0704 마이페이지 전체보기 화면에서 페이징 추가

--- src/main/front/src/views/user/MypageUserLectureDetail.vue
+++ src/main/front/src/views/user/MypageUserLectureDetail.vue
@@ -1,4 +1,3 @@
-<!-- npm install v-pagination-3 설치해야함 -->
 <template>
     <b-container>
         <div class="contents">
@@ -8,7 +7,7 @@
                 </h3>
             </div>
         </div>
-       <!-- <div v-for="(item, index) in lectureDetailInfo" :key="index"> v-for문 넣기-->
+         <!--<div v-for="(item, index) in paginatedData" :key="index">-->
             <b-container class="w-75 ms-auto py-5">
                     <b-container class="border rounded-3 py-3 mb-2">
                         <p class="fw-bold">프로그래밍 배워봅시다</p>
@@ -37,18 +36,67 @@
                                 </p>
                          </b-container>
                 </b-container>
-          <!--  </div>-->
-        </b-container>
-    
+            <!--</div>-->
+          <div class="btn-cover">
+            <b-button :disabled="pageNum === 0" @click="prevPage" class="page-btn">
+                이전
+            </b-button>
+            <span class="page-count">{{ pageNum + 1 }} / {{ pageCount }} 페이지</span>
+            <b-button :disabled="pageNum >= pageCount - 1" @click="nextPage" class="page-btn">
+                다음
+            </b-button>
+          </div>
+    </b-container>
 </template>
 <script>
 export default {
     name : "mypageAll",
     data() {
        return {
-           userName : this.$store.state.nickname
+           userName : this.$store.state.nickname,
+           pageNum : 0,
+           listArray : [],
+           pageSize : 10
+        }
+    },
+   methods : {
+        nextPage () {
+            this.pageNum += 1;
+        },
+        prevPage () {
+            this.pageNum -= 1;
+        }
+    },
+    computed : {
+        pageCount () {
+            let listLeng = this.listArray.length,
+            listSize = this.pageSize,
+            page = Math.floor(listLeng / listSize);
+            
+            if (listLeng % listSize > 0) page += 1;
+
+            return page;
+        },
+        paginatedData () {
+            const start = this.pageNum * this.pageSize,
+            end = start + this.pageSize;
+        return this.listArray.slice(start,end)
         }
     }
 }
 
 </script>
+<style>
+.btn-cover {
+  margin-top: 1.5rem;
+  text-align: center;
+}
+.btn-cover .page-btn {
+  width: 5rem;
+  height: 2rem;
+  letter-spacing: 0.5px;
+}
+.btn-cover .page-count {
+  padding: 0 1rem;
+}
+</style>
--- src/main/java/com/care/bedu/user/controller/MemberMypageController.java
+++ src/main/java/com/care/bedu/user/controller/MemberMypageController.java
@@ -3,8 +3,10 @@ package com.care.bedu.user.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
