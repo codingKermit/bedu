3abbcfd4832ff5e0476823154b40c77b323e463c
juyoung1222 <jjurong1222@gmail.com>
2023-07-12 16:31:08 +0900

    0712 마이페이지 페이징 처리 개발중

--- src/main/front/src/views/user/MypageUserLectureDetail.vue
+++ src/main/front/src/views/user/MypageUserLectureDetail.vue
@@ -7,66 +7,56 @@
                 </h3>
             </div>
         </div>
-         <!--<div v-for="(item, index) in paginatedData" :key="index">-->
+         <div v-for="(item, index) in paginatedData" :key="index">
             <b-container class="w-75 ms-auto py-5">
                     <b-container class="border rounded-3 py-3 mb-2">
                         <p class="fw-bold">프로그래밍 배워봅시다</p>
                         <p>
-                            <span>강좌이름 : </span> {{ lecttitle }}
+                            <span>강좌이름 : </span> {{ item.title }}
                         </p>
                         <p>
-                            <span>강의설명 : </span> {{ lectdesc }}
+                            <span>강의설명 : </span> {{ item.lect_desc }}
                         </p>
                         <p>
-                            <span>수강기간 : </span> {{ lectperiod }}
+                            <span>수강기간 : </span> {{ item.lect_period }}
                         </p>
                     </b-container>
             </b-container>
-                    <b-container class="w-75 ms-auto py-5">
-                        <b-container class="border rounded-3 py-3 mb-2">
-                                <p class="fw-bold">프로그래밍 배워봅시다</p>
-                                <p>
-                                    <span>강좌이름 : </span> {{ lecttitle }}
-                                </p>
-                                <p>
-                                    <span>강의설명 : </span> {{ lectdesc }}
-                                </p>
-                                <p>
-                                    <span>수강기간 : </span> {{ lectperiod }}
-                                </p>
-                         </b-container>
-                </b-container>
+        </div>
                 <div class="btn-cover">
-                    <b-button :disabled="pageNumber === 0" @click="prevPage" class="page-btn">
+                    <b-button :disabled="pageNumber === 0" @click="prevPage()" class="page-btn">
                         이전
                     </b-button>
-                    <span class="page-count">{{ pageNumber + 1 }} / {{ pageCount }} 페이지</span>
-                    <b-button :disabled="pageNumber >= pageCount - 1" @click="nextPage" class="page-btn">
+                    <span class="page-count">{{ pageNumber + 1 }} / {{ pageCount === 0 ? 1 : pageCount }} 페이지</span>
+                    <b-button :disabled="pageNumber >= pageCount - 1" @click="nextPage()" class="page-btn">
                         다음
                     </b-button>
                 </div>
-      <!--</div>-->
-</b-container>
+        </b-container>
 </template>
 <script>
 export default {
     name : "mypageAll",
     data() {
        return {
+           item : {
+                title : '',
+                lect_desc : '',
+                lect_period : ''
+           },
            userNum : this.$store.state.usernum,
            userId : this.$store.state.nickname,
+           userid : this.$store.state.email,
            pageNumber : 0,
-           listArray : [],
-           numOfPage : 10,
-           list : [],         //수강내역 전체 데이터
+           //listArray에 데이터 들어가는지 테스트중
+           listArray : [{title : '프로그래밍이지요이지요'},{title : '프로그래밍이지요'},{title : '프로그래밍'},{title : '프로그래밍'},{title : '프로그래밍'},{title : '프로그래밍'}], //수강내역 전체 데이터
+           numOfPage : 5,
            lectureInfo : {}, //화면에 노출되는 수강내역 데이터
-           lectureCount : 0  //수강내역 전체보기 출력
-         
+           lectureCount : 0,  //수강내역 전체보기 출력
         }
     }, 
     mounted(){
-        this.getLectureList();
-
+        this.getLectureAllList();
     },
     methods : {
         nextPage () {
@@ -75,46 +65,44 @@ export default {
         prevPage () {
             this.pageNumber -= 1;
         },
-         /* 마이페이지 홈(유저아이디 가져오기, 데이터 출력) */
-         getLectureList(){
-            const userNum = this.$store.getters.getUsernum;
+        /* 마이페이지 홈(유저아이디 가져오기, 데이터 출력) */
+         getLectureAllList(){
+            const userid = this.$store.getters.getEmail;
             let data = []
-            this.$axiosSend('get','/api/mypageAll',{userNum: userNum},true)
+            this.$axiosSend('get','/api/mypageAll',{userid: userid},true)
             .then((res)=>{
-                this.list = res.data;
-                for(var i = 0; i < this.list.length; i++) {
-                        data.push(res.data.Data[i]);
+                for(var i = 0; i < this.numOfPage; i++) {
+                        data.push(this.listArray[i]);
                 }
-                this.list = res.data.Data
-                this.lectureInfo = data
-                this.lectureCount = this.list
+                console.log(res)
+                
+                this.lectureIfo = data
+                this.lectureCount = this.listArray.length
             })
             .catch((err)=>{
                 console.log(err)
             })
-            console.log("######",this.list)
             console.log("######22222",this.lectureInfo)
             console.log("#####!!!!",this.lectureCount)
-        }
+        },
     },
     computed : {
-        pageCount () {
+      pageCount() {
             let listLeng = this.listArray.length,
             listSize = this.numOfPage,
             page = Math.floor(listLeng / listSize);
             
             if (listLeng % listSize > 0) page += 1;
-            console.log("##### page",page)
+            
             return page;
         },
-        paginatedData () {
-            const startNo = (this.pageNumber - 1) * this.numOfPage,
-            end = startNo + this.numOfPage;
-        return this.listArray.slice(startNo,end)
+        paginatedData() {
+            const startNo = this.pageNumber * this.numOfPage;
+            const end = startNo + this.numOfPage;
+            
+            return this.listArray.slice(startNo,end)
+            
         },
-        rows() {
-        return this.items.length
-      }
     }
 }
 
