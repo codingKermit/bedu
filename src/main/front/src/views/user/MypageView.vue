e2ab3e802ab77178b68276e2c40f191c67e827fc
juyoung1222 <jjurong1222@gmail.com>
2023-07-14 14:02:42 +0900

    마이페이지 북마크 화면개발 및 쿼리수정

--- src/main/front/src/views/user/MypageView.vue
+++ src/main/front/src/views/user/MypageView.vue
@@ -1,9 +1,6 @@
 <template>
     <b-container>
-        <b-container>
-            <p class="fs-4 fw-bold">마이페이지</p>
-        </b-container>
-        <p class="fs-4 fw-bold">현재 수강정보</p>
+        <p class="fs-4 fw-bold" style="padding-top: 5rem;">현재 수강정보</p>
             <b-container class="text-dark fw-bold">
                     <!-- v-if문 이용하여  수강내역이 없을때 수강내역이 없다는 문구 보이게-->
                     <div class="curr-subjectInfo" v-if="lectureListFirst == 0">
@@ -11,7 +8,7 @@
                             <p style="text-align: center;">수강내역이 없습니다.</p>
                        </b-container>
                     </div>
-                    <!--v-else를 이용하여 userList가 있고 수강내역이 있을때이라면 현재 수강정보가 보이게-->
+                    <!--v-else를 이용하여 수강내역이 있을때이라면 현재 수강정보가 보이게-->
                     <div v-else class="lecture">
                         <div style="text-align: right;">
                             <a  @click="getLectureCount" style="cursor:pointer; text-align: right;">전체보기</a>
@@ -37,27 +34,53 @@
                             </div>
                         </div>
                     </div>
-                </b-container>
             </b-container>
-        <!--
-        <div style="bottom;">
-            <b-container>
-                <p class="fs-4 fw-bold">북마크</p>
-                    <b-container class="w-75 ms-auto py-5">
-                            <div class="text-dark fw-bold">
-                            <p style="text-align: center;">북마크내역이 없습니다.</p>
-                            </div>
-                            <div  class="bookmark">
-                                <div class="col-xl-3 col-lg-4 col-md-6">
-                                    <div class="lect text-start" style="width:18rem; border:0px">
-                                        
+            <b-container class="mypagebookmark">
+                <div class="mypageBookmark">
+                    <b-container>
+                        <p class="fs-4 fw-bold">북마크</p>
+                            <b-container class="w-75 ms-auto py-5">
+                                    <!-- v-if문 이용하여  북마크 내역이 없을때 북마크내역이 없다는 문구 보이게-->
+                                    <div class="curr-subjectInfo" ><!--v-if="bookmarkFirst == 0"-->
+                                        <b-container class="w-75 ms-auto py-5">
+                                                <p style="text-align: center;">북마크가 없습니다.</p>
+                                        </b-container>
                                     </div>
-                                </div>
-                            </div>
-                        </b-container>
+                                   <!--v-else를 이용하여 수강내역이 있을때이라면 현재 수강정보가 보이게-->
+                                   <!-- <div  class="bookMark"> --><!--v-else-->
+                                        <!--
+                                        <div style="text-align: right;">
+                                            <a  @click="getLectureCount" style="cursor:pointer; text-align: right;">전체보기</a>
+                                        </div>
+                                        <div class="mypagebookmarkcontainer" style="float:left;" v-for="(item, index) in bookmarkFirst" :key="index">
+                                            <div class="lect text-start">-->
+                                            <!--  링크걸어서 화면 이동 테스트중 -->
+                                                <!--<b-link class="text-decoration-none text-body h-100 d-block" :to='"/mypageAll"'>
+                                                    <div class="mypagebookmarkInfo">
+                                                        <div class="mypagebookmarkContain">
+                                                            <p class="fw-bold">
+                                                                <span>강좌이름 : </span> {{ item.title }}
+                                                            </p>
+                                                            <p class="fw-bold">
+                                                                <span>강의설명 : </span> {{ item.lectDesc }}
+                                                            </p>
+                                                            <p class="fw-bold">
+                                                                <span>수강기간 : </span> {{ item.lectPeriod }}
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </b-link>
+                                            </div>
+                                        </div>
+                                    </div>-->
+                            </b-container>
                     </b-container>
-        </div>-->
+                </div>
+        </b-container>        
+    </b-container>
+    
 </template>
+
 <style>
 .mypagecontainer{
     float:left;
@@ -67,7 +90,11 @@
     text-align : left;
     border : 1px solid rgb(15, 10, 1);
 }
-
+.mypagebookmark{
+    margin-top : 20rem;
+    display: flex;
+    flex-direction: column;
+}
 </style>
 <script>
 
